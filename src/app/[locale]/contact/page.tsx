5ac07c0bb1b917d9e8b1ba0018b9e23ede359da5
chungvu <chungvd.it@gmail.com>
2025-10-10 17:08:57 +0700

    a little change

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,22 +1,27 @@
 import PageBreadcrumb from "@/components/PageBreadcrumb";
-export default function ContactPage() {
+import type { Locale } from "@/lib/i18n";
+import { getDictionary } from "@/lib/getDictionary";
+export default async function ContactPage({ params }: { params: { locale: Locale } }) {
+    const { locale } = params;
+    const t = await getDictionary(locale);
     return (
         <>
-            <PageBreadcrumb pageName="Contact Us" />
+            <PageBreadcrumb pageName={t.footer.contact_us_breadcrumb} />
             <div className="container">
                 <div className="contact-area mg-top-120 mb-120">
                     <div className="row g-0 justify-content-center">
                         <div className="col-lg-7">
                             <form className="contact-form text-center">
-                                <h3>GET A QUOTE</h3>
+                                <h3>{t.footer.get_a_quote}</h3>
+
                                 <div className="row">
-                                    <div className="col-md-6">
+                                    <div className="col-md-12">
                                         <div className="single-input-inner">
                                             <label>
 
                                                 <i className="fa fa-user" />
                                             </label>
-                                            <input type="text" placeholder="Your name" />
+                                            <input type="text" placeholder={t.footer.your_name} />
                                         </div>
                                     </div>
 
@@ -25,7 +30,7 @@ export default function ContactPage() {
                                             <label>
                                                 <i className="fa fa-envelope" />
                                             </label>
-                                            <input type="text" placeholder="Your email" />
+                                            <input type="text" placeholder={t.footer.your_email} />
                                         </div>
                                     </div>
 
@@ -35,41 +40,39 @@ export default function ContactPage() {
 
                                                 <i className="fas fa-calculator" />
                                             </label>
-                                            <input type="text" placeholder=" Phone number" />
+                                            <input type="text" placeholder={t.footer.phone_number} />
                                         </div>
                                     </div>
 
-                                    <div className="col-md-6">
+                                    {/* <div className="col-md-6">
                                         <div className="single-select-inner">
                                             <label>
 
                                                 <i className="far fa-file-alt" />
                                             </label>
 
-                                            {/* Hidden select (for nice-select plugin markup) */}
-                                            <select className="single-select" style={{ display: "none" }}>
-                                                <option>Subject</option>
-                                                <option value="1">Some option</option>
-                                                <option value="2">Another option</option>
-                                            </select>
+                                    <select className="single-select" style={{ display: "none" }}>
+                                        <option>Subject</option>
+                                        <option value="1">Some option</option>
+                                        <option value="2">Another option</option>
+                                    </select>
 
-                                            {/* Visible fake select structure */}
-                                            <div className="nice-select single-select" tabIndex={0}>
-                                                <span className="current">Subject</span>
-                                                <ul className="list">
-                                                    <li data-value="Subject" className="option selected">
-                                                        Subject
-                                                    </li>
-                                                    <li data-value="1" className="option">
-                                                        Some option
-                                                    </li>
-                                                    <li data-value="2" className="option">
-                                                        Another option
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
+                                    <div className="nice-select single-select" tabIndex={0}>
+                                        <span className="current">Subject</span>
+                                        <ul className="list">
+                                            <li data-value="Subject" className="option selected">
+                                                Subject
+                                            </li>
+                                            <li data-value="1" className="option">
+                                                Some option
+                                            </li>
+                                            <li data-value="2" className="option">
+                                                Another option
+                                            </li>
+                                        </ul>
                                     </div>
+                                </div>
+                        </div> */}
 
                                     <div className="col-12">
                                         <div className="single-input-inner">
@@ -77,14 +80,14 @@ export default function ContactPage() {
 
                                                 <i className="fas fa-pencil-alt" />
                                             </label>
-                                            <textarea placeholder="Write massage"></textarea>
+                                            <textarea placeholder={t.footer.write_message}></textarea>
                                         </div>
                                     </div>
 
                                     <div className="col-12">
                                         <a className="btn btn-base" href="#">
                                             {" "}
-                                            SEND MESSAGE{" "}
+                                            {t.footer.send_message}{" "}
                                         </a>
                                     </div>
                                 </div>
@@ -94,38 +97,38 @@ export default function ContactPage() {
                         <div className="col-lg-5">
                             <div className="contact-information-wrap">
                                 <h3 className="contact-heading" style={{ color: "#2c4397" }}>
-                                    CONTACT INFORMATION
+                                    {t.footer.contact_information}
                                 </h3>
 
                                 <div className="single-contact-info-wrap">
-                                    <h6 className="contact-label">Contact Number:</h6>
+                                    <h6 className="contact-label">{t.footer.contact_number}</h6>
                                     <div className="media">
                                         <div className="icon contact-icon-phone">
 
                                             <i className="fa fa-phone-alt" />
                                         </div>
                                         <div className="media-body">
-                                            <p className="contact-text">+91 97 4660 9023</p>
-                                            <p className="contact-text">+91 97 4660 9023</p>
+                                            <p className="contact-text">{t.footer.phone1}</p>
+                                            <p className="contact-text">{t.footer.phone2}</p>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="single-contact-info-wrap">
-                                    <h6 className="contact-label">Mail Address:</h6>
+                                    <h6 className="contact-label">{t.footer.mail_address}</h6>
                                     <div className="media">
                                         <div className="icon contact-icon-envelope">
                                             <i className="fa fa-envelope" />
                                         </div>
                                         <div className="media-body">
-                                            <p className="contact-text">info@expeditorsint.com</p>
-                                            <p className="contact-text">info@expeditorsglobal.com</p>
+                                            <p className="contact-text">{t.footer.email1}</p>
+                                            <p className="contact-text">{t.footer.email2}</p>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div className="single-contact-info-wrap mb-0">
-                                    <h6 className="contact-label">Office Location:</h6>
+                                    <h6 className="contact-label">{t.footer.office_location}</h6>
                                     <div className="media">
                                         <div className="icon contact-icon-map">
 
@@ -133,21 +136,18 @@ export default function ContactPage() {
                                         </div>
                                         <div className="media-body">
                                             <p className="contact-text">
-                                                Office No.504, 5th Floor, Mina Commercial Center, Grand Hotel Building, Hindawiya Dist.
-                                                Mina Road, Jeddah 22321, KSA
+                                                {t.footer.address}
                                             </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>{/* col-right */}
-                    </div>
-                </div>
-            </div>
+                    </div >
+                </div >
+            </div >
             <div className="contact-g-map">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d29208.601361499546!2d90.3598076!3d23.7803374!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1589109092857!5m2!1sen!2sbd"
-                />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3544.2575342719074!2d106.66635682502593!3d20.841668580761187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7bca432acb4f%3A0x1e6850f2cadfc7e1!2zVMOyYSBuaMOgIELhuqFjaCDEkOG6sW5n!5e1!3m2!1sen!2s!4v1760076786114!5m2!1sen!2s" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </>
     );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,10 +18,8 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                                         <img src="/assets/img/map-marker.png" alt="img" />
                                     </div>
                                     <div className="details">
-                                        <h6>OFFICE ADDRESS:</h6>
-                                        <p>Expeditors International Contracting Company Jeddah, Hindawiyah</p>
-                                        <br />
-                                        <p>Expeditors Logistics Consultancy<br /> Dubai, Karama</p>
+                                        <h6>{t.footer.office_address}</h6>
+                                        <p>{t.footer.address}</p>
                                     </div>
                                 </div>
                             </div>
@@ -30,12 +28,13 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                                 <div className="single-footer-top">
                                     <div className="icon"><img src="/assets/img/phone.png" alt="img" /></div>
                                     <div className="details">
-                                        <h6>CONTACT US:</h6>
-                                        <p>info@expeditorsint.com</p>
-                                        <p>info@expeditorsglobal.com</p>
-                                        <p>+966 575311289</p>
-                                        <p>+966 556328009</p>
-                                        <p>+971 581444350</p>
+                                        <h6>{t.footer.contact_us}</h6>
+                                        <p>{t.footer.person1}</p>
+                                        <p>{t.footer.email1}</p>
+                                        <p>{t.footer.phone1}</p>
+                                        <p>{t.footer.person2}</p>
+                                        <p>{t.footer.email2}</p>
+                                        <p>{t.footer.phone2}</p>
                                     </div>
                                 </div>
                             </div>
@@ -44,8 +43,8 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                                 <div className="single-footer-top after-none">
                                     <div className="icon"><img src="/assets/img/clock.png" alt="img" /></div>
                                     <div className="details">
-                                        <h6>WORKING HOURS:</h6>
-                                        <p>Sunday - Thursday</p>
+                                        <h6>{t.footer.working_hours}</h6>
+                                        <p>{t.footer.hours}</p>
                                     </div>
                                 </div>
                             </div>
@@ -63,9 +62,7 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                                 </div>
                                 <div className="details">
                                     <p>
-                                        Expeditors International Contracting Company is a trusted supplier of industrial products and services
-                                        in Dubai. We specialize in providing customized solutions and a diverse range of products that meet
-                                        international standards.
+                                        {t.footer.slogan}
                                     </p>
                                     <ul className="social-media style-border">
                                         <li><a href="#"><i className="fab fa-facebook-f" /></a></li>
@@ -81,9 +78,9 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                             <div className="widget widget_nav_menu">
                                 <h4 className="widget-title">{t.footer.useful}</h4>
                                 <ul>
-                                    <li><Link href={`/${locale}`}><i className="fa fa-arrow-right" /> {t.nav.home}</Link></li>
+                                    <li><Link href={`/${locale}/`}><i className="fa fa-arrow-right" /> {t.nav.home}</Link></li>
                                     <li><Link href={`/${locale}/about`}><i className="fa fa-arrow-right" /> {t.nav.about}</Link></li>
-                                    <li><Link href={`/${locale}/service`}><i className="fa fa-arrow-right" /> {t.nav.services}</Link></li>
+                                    <li><Link href={`/${locale}/services`}><i className="fa fa-arrow-right" /> {t.nav.services}</Link></li>
                                     <li><Link href={`/${locale}/contact`}><i className="fa fa-arrow-right" /> {t.nav.contact}</Link></li>
                                 </ul>
                             </div>
@@ -124,7 +121,7 @@ export default async function Footer({ params }: { params: { locale: Locale } })
                     <div className="row">
                         <div className="col-lg-6 text-lg-start text-center">
                             <div className="copyright-area">
-                                <p>© Copyright 2023 By <a href="#">Expeditors Global</a>, All right reserved.</p>
+                                <p>© Copyright 2025 By <a href="#">Expeditors Global</a>, All right reserved.</p>
                             </div>
                         </div>
                     </div>
